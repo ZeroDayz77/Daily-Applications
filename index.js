31cfe02ce3350b3c98de3df2bec28fa56ba41741
zerodayz77 <charisyt26@gmail.com>
2023-06-27 17:26:05 -0400

    I did not realise how many uncommitted changes I had made in the past, but none the less, I will be coming back to this project. I have added node and express for routing and also fixed a few non-functional parts of the code, quality of life additions and a footer

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 //script for the home page and all other linking to other pages.
 //code starts here:
+const express = require('express')
+const router = express.Router();
+const path = require('path');
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/views')));
+app.use('/', router);
+
+router.get("/",function(req,res){
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+  });
+   
+router.get("/calendar",function(req,res){
+    res.sendFile(path.join(__dirname+'/views/calendar.html'));
+  });
+   
+router.get("/todo",function(req,res){
+    res.sendFile(path.join(__dirname+'/views/todo.html'));
+  });
+   
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
+
